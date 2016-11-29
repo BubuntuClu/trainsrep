09505f22ad93f9421bc1fb3abfb120b20c6fa261
aiidko <aiidko@mail.ru>
2016-11-29 23:05:02 +0500

    route CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128172839) do
+ActiveRecord::Schema.define(version: 20161129172306) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -18,8 +18,14 @@ ActiveRecord::Schema.define(version: 20161128172839) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "routes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "trains", force: :cascade do |t|
-    t.integer  "number"
+    t.string   "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
