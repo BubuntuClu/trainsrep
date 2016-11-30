a6312aedb58ab65bded71279619d9210d463a0a5
aiidko <aiidko@mail.ru>
2016-11-30 22:36:38 +0500

    delete space

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :train
-
-
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
 end
