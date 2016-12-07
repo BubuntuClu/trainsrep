53f5cc0d9412603491b6b031a374eecf9127c423
aiidko <aiidko@mail.ru>
2016-12-07 21:53:37 +0500

    returned rs with admin

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,15 +1,17 @@
 class TicketsController < ApplicationController
+  before_action :authenticate_user!, only: :create
   before_action :hidden_params, only: [:new]
 
   def show
+    @ticket = Ticket.find(params[:id])
   end
 
   def create
-    @ticket = Ticket.new(ticket_params)
+    @ticket = current_user.tickets.new(ticket_params)
     if @ticket.save
       redirect_to @ticket
     else
-      render :show
+      render :new
     end
   end
 
